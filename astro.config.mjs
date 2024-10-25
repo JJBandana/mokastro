1acc07a0c496e4fb92d538fa050c80b59f6b0cde
Juan Briasco <jj.briasco@gmailcom>
2024-10-25 23:57:08 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  outDir: "./docs",
+	site: 'https://jjbandana.github.io/mokastro',
+});
