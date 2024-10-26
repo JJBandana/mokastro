14fb215333afbfb4c0feca180689935bc3d3f933
Juan Briasco <jj.briasco@gmailcom>
2024-10-26 16:07:09 +0200

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  outDir: "./docs",
 	site: 'https://jjbandana.github.io',
 });
