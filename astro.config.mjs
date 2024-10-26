e7bc0e0d8af997420a9826c19446f6bfaa5c8f93
Juan Briasco <jj.briasco@gmailcom>
2024-10-26 21:36:50 +0200

    fixed counter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jjbandana.github.io',
+  outDir: "./docs",
+  output: "static",
+  build: {
+    assets: "astro"
+  }
 });
